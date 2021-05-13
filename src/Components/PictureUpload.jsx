5891d009c3330618d479772da7a622828c26cb8c
Yossef Vashdi <76695165+yossef-vashdi@users.noreply.github.com>
2021-05-13 05:15:13 +0300

    yarn build

--- src/Components/PictureUpload.jsx
+++ src/Components/PictureUpload.jsx
@@ -2,15 +2,31 @@ import React from "react";
 import { useState } from "react";
 import Modal from "react-modal";
 import { createPicture } from "../lib/api";
+// import { Link } from "react-router-dom";
 
 const PictureUpload = () => {
   const [showValidationError, setShowValidationError] = useState(false);
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [picture, setPicture] = useState();
+  const [picturesData, setPicturesData] = useState([]);
 
   async function addNewPicture(picture) {
-    const data = await createPicture(picture);
-    return data;
+    try {
+      const data = await createPicture(picture);
+      if (data && !isPictureAlreadyExists(picturesData, data.fileName)) {
+        const arr = [data, ...picturesData];
+        setPicturesData(arr);
+      } else {
+        setModalIsOpen(false);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  function isPictureAlreadyExists(picturesData, pictureName) {
+    const data = picturesData.find((e) => e.fileName === pictureName);
+    return data ? true : false;
   }
 
   function closeButton() {
@@ -26,6 +42,7 @@ const PictureUpload = () => {
     event.preventDefault();
     if (picture) {
       try {
+        setShowValidationError(false);
         await addNewPicture(picture);
         setModalIsOpen(true);
       } catch (err) {
@@ -44,9 +61,10 @@ const PictureUpload = () => {
             className=" justify-content-center p-5 bg-white mt-2 "
             onSubmit={(event) => handleFormSubmit(event)}
           >
-            <h1 className="text-center mb-4">Picture Upload</h1>
-            <div className="row mb-3">
-              {/* <label className="col-sm-2 col-form-label">Picture</label> */}
+            <h1 className="text-center mb-4" id="p">
+              Picture Upload
+            </h1>
+            <div className="row mb-3 ">
               <div className="">
                 <div className=" input-group mb-1">
                   <input
@@ -55,12 +73,6 @@ const PictureUpload = () => {
                     onChange={(e) => uploadPicture(e.target.files[0])}
                     id="inputGroupFile02"
                   />
-                  {/* <label
-                    className="input-group-text "
-                    htmlFor="inputGroupFile02"
-                  >
-                    Upload
-                  </label> */}
                 </div>
               </div>
             </div>
@@ -81,7 +93,10 @@ const PictureUpload = () => {
               )}
 
               <div className="d-flex justify-content-center">
-                <button className="btn btn-dark rounded-pill mt-1 " type="submit">
+                <button
+                  className="btn btn-dark rounded-pill mt-1 "
+                  type="submit"
+                >
                   Upload
                 </button>
               </div>
@@ -89,6 +104,33 @@ const PictureUpload = () => {
           </form>
         </div>
       </div>
+      <ul className=" list-unstyled">
+        {picturesData &&
+          picturesData.map((item) => (
+            <li className=" ">
+              <div className="card mb-4" width="540">
+                <div className="row g-0">
+                  <div className="col-md-4">
+                    <img
+                      src={item.path}
+                      className="card-img-top"
+                      alt="..."
+                      height="300"
+                    />
+                  </div>
+                  {/* <p>{item.id}</p> */}
+                  <div className="col-md-8">
+                    <div className="card-body">
+                      <h1 className="card-title">{item.title}</h1>
+                      <p className="card-text">{item.text}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+          ))}
+      </ul>
+
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={() => setModalIsOpen(false)}
@@ -96,7 +138,7 @@ const PictureUpload = () => {
         className="pt-5 w-25 m-auto mt-5"
       >
         <div
-          className="d-flex justify-content-center alert alert-warning mt-2 alert-dismissible fade show"
+          className="d-flex justify-content-center alert alert-warning mt-2 alert-dismissible fade show p-5"
           role="alert"
         >
           The picture was uploaded!
